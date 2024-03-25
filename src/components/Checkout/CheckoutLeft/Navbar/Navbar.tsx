ec9da09d6331483c9bcb521e8f71693cfee37fc8
emanuelnponce <emanuel@wisboo.com>
2024-03-25 18:30:39 -0300

    feat: implemented left navbar UI

--- src/components/Checkout/CheckoutLeft/Navbar/Navbar.tsx
+++ src/components/Checkout/CheckoutLeft/Navbar/Navbar.tsx
@@ -1,5 +1,16 @@
+import Pill from '@/components/Shared/Pill/Pill';
+import Image from 'next/image';
+import { FiArrowLeft } from 'react-icons/fi';
+
 function Navbar() {
-  return <div></div>;
+  return (
+    <div className="flex gap-2 items-center w-full">
+      <FiArrowLeft />
+      <Image src="/logo.png" width={28} height={28} alt="Logo" />
+      <span className="text-sm">Powdur</span>
+      <Pill text="TEST MODE" bgColor="yellow" textColor="orange" />
+    </div>
+  );
 }
 
 export default Navbar;

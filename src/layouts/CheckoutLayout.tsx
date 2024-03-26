0bb9068bb15b80c81ed54b7a0c1efb9eabfd99bb
emanuelnponce <emanuel@wisboo.com>
2024-03-26 12:46:01 -0300

    feat: implemented background sync

--- src/layouts/CheckoutLayout.tsx
+++ src/layouts/CheckoutLayout.tsx
@@ -3,11 +3,14 @@ import { ReactNode } from 'react';
 interface CheckoutLayoutProps {
   children: ReactNode[];
 }
+import { selectBrandColor } from 'checkoutPagePreview/customizationSlice';
+import { useSelector } from 'react-redux';
 
 export default function CheckoutLayout({ children }: CheckoutLayoutProps) {
+  const bgColor: string = useSelector(selectBrandColor);
   return (
     <div className="flex flex-wrap h-full">
-      <div className="w-full md:w-1/2 flex justify-center bg-gray-200">
+      <div className="w-full md:w-1/2 flex justify-center" style={{ backgroundColor: bgColor }}>
         <div className="max-w-[80%] flex grow mt-10">{children[0]}</div>
       </div>
       <div className="w-full md:w-1/2  flex justify-center shadow-xl">
